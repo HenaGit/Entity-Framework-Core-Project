b5f8038e16e9e3a17a0c4747acb1e3bac2399c31
henokg <heni.gebrehiwot@gmail.com>
2023-04-02 14:51:40 -0600

    AddRelationBookAndCategory

--- WizLib/WizLib_Model/Models/Book.cs
+++ WizLib/WizLib_Model/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +19,8 @@ namespace WizLib_Model.Models
         public string ISBN { get; set; }
         [Required]
         public double Price { get; set; }
+        [ForeignKey("Category")]
+        public int Category_Id { get; set; }
+        public Category Category { get; set; }
     }
 }
