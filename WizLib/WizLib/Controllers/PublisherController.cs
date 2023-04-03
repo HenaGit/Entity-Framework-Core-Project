4d5514a02abad2206a20b7c1ada8fc0b4eea288b
henokg <heni.gebrehiwot@gmail.com>
2023-04-03 13:24:12 -0600

    Section 5 - Setup Author and Publisher

---
+++ WizLib/WizLib/Controllers/PublisherController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Publisher obj)
+        {
+
+            return View();
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
